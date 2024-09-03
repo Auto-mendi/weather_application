3d475f61a08ba1f18cd147bea6b49b2ea1785bff
Mpho <ndumompho15@gmail.com>
2024-09-03 22:11:06 +0200

    first commit

---
+++ lib/routes/routes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/views/main_page.dart';
+//import '../views/main_page.dart';
+
+class RouteManager {
+  static const String mainPage = '/';
+  static const String weatherPage = '/weatherPage';
+  static const String detailsPage = '/deatailsPage';
+
+  static Route<dynamic> routes(RouteSettings settings) {
+    switch (settings.name) {
+      case mainPage:
+        return MaterialPageRoute(
+          builder: (context) => const MainPage(),
+        );
+
+      // case weatherPage:
+      //   return MaterialPageRoute(
+      //     builder: (context) => const MainPage(),
+      //   );
+
+      // case detailsPage:
+      //   return MaterialPageRoute(
+      //     builder: (context) =>  WeatherDetails(),
+      //   );
+
+      default:
+        throw const FormatException(
+            'Route not found! Please check for correct route');
+    }
+  }
+}
